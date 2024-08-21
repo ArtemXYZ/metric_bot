49f41b9d974609ff49578998ef523cd275ea7bdc
Poznyishev.AA <Poznyishev.AA@dns-shop.ru>
2024-08-21 10:43:25 +0400

    Исправлены баги логи, созданы новые запросы, но есть ошибки

--- db_connect/query_builder.py
+++ db_connect/query_builder.py
@@ -29,7 +29,7 @@ async def check_tg_id(tg_id, session:AsyncSession):
     """
     try:
         # Форматируем SQL запрос, если есть аргументы для форматирования
-        formatted_query = get_tg_id_jar.format(tg_id) #
+        formatted_query = tg_id_jar.format(tg_id) #
 
         # Извлекаем данные:
         result_tmp = await session.execute(text(formatted_query))
@@ -37,27 +37,197 @@ async def check_tg_id(tg_id, session:AsyncSession):
         return results
     except Exception as error_text:
         print(f'Ошибка в "check_tg_id": {error_text}')
-        return None
+        return 'error'
+
 
 async def get_user_data(tg_id, session: AsyncSession):
     """
         Функция для извлечения данных пользователя. На вход поступает сессия (готовое подключение к бд) и \
         телеграмм айди.  Далее, осуществляется запрос.
         sql = get_user_data_gp_mart_sv
-        Вернет:
+        Вернет: ('235UI',)
     """
     try:
-        formatted_query = get_user_data_gp_mart_sv.format(tg_id)  #
-        # Извлекаем данные:
-        result_tmp = await session.execute(text(formatted_query))
+        formatted_query = user_data_gp_mart_sv.format(tg_id)  #
+        result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
         results = result_tmp.fetchone()  # Возвращает одну строку результата в виде кортежа или None
+        # await session.dispose()  # Закрытие соединения вручную. Важно! Если не закрыть соединение,\
+        # будут ошибки!
         return results
     except Exception as error_text:
-        print(f'Ошибка в "get_user_data": {error_text}')
-        return 'error'
+        print(f'Ошибка в результате запроса в функции "get_user_data": {error_text}')
+        return None
+
+
+# ----------------------------- KPD --------------------------------
+async def get_kpd_user(user_guid, session: AsyncSession):
+    """
+        Функция для извлечения данных о КПД пользователя. На вход поступает сессия (готовое подключение к бд, Джарвис)\
+        и гуид.
+        sql = get_kpd_user
+        Вернет:
+    """
+
+    try:
+        formatted_query = kpd_user.format(user_guid)  #
+        result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+        results = result_tmp.fetchone()  # Возвращает одну строку или None
+        return results
+    except Exception as error_text:
+        print(f'Ошибка в результате запроса в функции "get_kpd_user": {error_text}')
+        return None
+
+
+# async def get_kpd_rating(user_guid, session: AsyncSession):
+#     """
+#         Функция для извлечения данных о рейтинге пользователя. На вход поступает сессия \
+#         (готовое подключение к бд, Джарвис) и гуид.
+#         sql = get_kpd_user
+#         Вернет:
+#     """
+#
+#     try:
+#         formatted_query = get_kpd_rating.format(user_guid)  #
+#         result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+#         results = result_tmp.fetchone()  # Возвращает одну строку или None
+#         return results
+#     except Exception as error_text:
+#         print(f'Ошибка в "get_user_data": {error_text}')
+#         return None
+
+
+async def get_kpd_rating_user(user_guid, session: AsyncSession):
+    """
+        Функция для извлечения рейтинга КПД пользователя в текущем месяце. На вход поступает сессия \
+        (готовое подключение к бд, Джарвис) и гуид.
+        sql = get_kpd_rating_user
+        Вернет:
+    """
+
+    try:
+        formatted_query = kpd_rating_user.format(user_guid)  #
+        result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+        results = result_tmp.fetchone()  # Возвращает одну строку или None (картеж) (1, 'Alice')
+        return results
+    except Exception as error_text:
+        print(f'Ошибка в результате запроса в функции "get_kpd_rating_user": {error_text}')
+        return None
+
+
+async def get_top5_kpd_rating_users(user_guid, session: AsyncSession):
+    """
+        Функция для извлечения Топ 5 пользователей по рейтингу в текущем месяце. На вход поступает сессия \
+        (готовое подключение к бд, Джарвис) и гуид.
+        sql = get_top5_kpd_rating_users
+        Вернет: массив: UserName, kpd, rating_kpd
+    """
+
+    try:
+        formatted_query = top5_kpd_rating_users.format(user_guid)  #
+        result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+        results = result_tmp.fetchall()  # Возвращает список кортежей: [(1, 'Alice'), (2, 'Bob')]
+        return results
+    except Exception as error_text:
+        print(f'Ошибка в результате запроса в функции "get_top5_kpd_rating_users": {error_text}')
+        return None
 
 
 
+async def get_top5end_kpd_rating_users(user_guid, session: AsyncSession):
+    """
+        Функция для извлечения Топ 5 пользователей по рейтингу в текущем месяце. На вход поступает сессия \
+        (готовое подключение к бд, Джарвис) и гуид.
+        sql = get_top5_kpd_rating_users
+        Вернет: массив: UserName, kpd, rating_kpd
+    """
+
+    try:
+        formatted_query = top5end_kpd_rating_users.format(user_guid)  #
+        result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+        results = result_tmp.fetchall()  # Возвращает список кортежей: [(1, 'Alice'), (2, 'Bob')]
+        return results
+    except Exception as error_text:
+        print(f'Ошибка в результате запроса в функции "get_top5end_kpd_rating_users": {error_text}')
+        return None
+
+
+
+
+
+
+# async def get_kpd_dinamic(user_guid, session: AsyncSession):
+#     """
+#         Функция для извлечения данных о рейтинге пользователя. На вход поступает сессия \
+#         (готовое подключение к бд, Джарвис) и гуид.
+#         sql = get_kpd_user
+#         Вернет:
+#     """
+#
+#     try:
+#         formatted_query = get_kpd_rating.format(user_guid)  #
+#         result_tmp = await session.execute(text(formatted_query)) # Извлекаем данные:
+#         results = result_tmp.fetchone()  # Возвращает одну строку или None
+#         return results
+#     except Exception as error_text:
+#         print(f'Ошибка в "get_user_data": {error_text}')
+#         return None
+
+
+
+
+
+
+
+
+
+
+
+# ---------------------------------------- Итог, сборка общих показателей рейтинга КПД:
+async def get_kpd_msg(user_guid, session: AsyncSession):
+    """
+        Функция для формирования данных о рейтинге КПД и др. данных. На вход поступает гуид.
+        Основная информация извлекается вложенными функциями.
+
+        Вернет: текст с показателями.
+    """
+    # try:
+
+    # 0. КПД пользователя:
+    kpd = await get_kpd_user(user_guid, session)
+    print(f'get_kpd_user: {kpd}')
+
+    # 1. Рейтинг конкретного пользователя в текущем месяце (картеж или None):
+    kpd_rating_user = await get_kpd_rating_user(user_guid, session)
+    print(f'kpd_rating_user: {kpd_rating_user}')
+
+    # 2. Топ 5 пользователей по рейтингу в текущем месяце (картеж или None):
+    top5_kpd_rating_users = await get_top5_kpd_rating_users(user_guid, session)
+    print(f'top5_kpd_rating_users: {top5_kpd_rating_users}')
+
+    # 3. Топ 5 пользователей худших по рейтингу (5 нижних позиций) в текущем месяце (картеж или None):
+    top5end_kpd_rating_users = await get_top5end_kpd_rating_users(user_guid, session)
+    print(f'top5end_kpd_rating_users: {top5end_kpd_rating_users}')
+
+
+    # rating_kpd = await get_kpd_rating(user_guid)
+    # print(f'get_kpd_rating": {rating_kpd}')
+
+
+    # kpd_dinamic = await get_kpd_dinamic(user_guid)
+    # print(f'kpd_dinamic: {kpd_dinamic}')
+
+
+
+
+
+
+
+
+
+        # return results
+    # except Exception as error_text:
+    #     print(f'Ошибка в "get_user_data": {error_text}')
+    #     return 'error'
 
 
 
--- sql/all_sql.py
+++ sql/all_sql.py
@@ -3,10 +3,10 @@
 """
 
 #  Тип данных столбца tg в базе данных — это строка, не пердавать в запросе значение в виде целого числа !
-get_tg_id_jar = """SELECT code as user_id FROM inlet.staff_for_bot WHERE tg = '{0}'"""
+tg_id_jar = """SELECT code as user_id FROM inlet.staff_for_bot WHERE tg = '{0}'"""
 
 
-get_user_data_gp_mart_sv = """
+user_data_gp_mart_sv = """
 SELECT
     emp_type as role, emp_guid as guid 
 FROM 
@@ -14,6 +14,217 @@ FROM
 WHERE
     emp_code_1c = '{0}'
 """
+
+# Вернет 1 значение
+kpd_user = """
+SELECT 
+        kpd 
+FROM 
+    (SELECT 
+            InvoiceTime + PaymentTime + ReturnTime + GivedTime + ArrivalTime + ShipmentTime + 
+            BillingTime + WorkCarTime + ReevaluationTime + DustingTime + CleaningTime + ServisTime +            
+            InventoryTime + GroupGoodsTime + (((InvoiceCnt * 2)*240)/3600) + OnlineOrdersTime + ActualTime +           
+            ExchangeTime + ZReportTime as fact,
+            ReportCard as tabel,
+            fact/ tabel as kpd
+    FROM 
+            ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+    WHERE 
+            UserGuid = '{0}'
+        and 
+            CurrentPeriod = toStartOfMonth(now())
+    ) kpd
+"""
+
+# Вернет массив
+# kpd_rating = """
+# with kpd as
+#         (
+#             SELECT
+#                     UserName,
+#                     UserGuid,
+#                     InvoiceTime + PaymentTime + ReturnTime + GivedTime + ArrivalTime + ShipmentTime +
+#                     BillingTime + WorkCarTime + ReevaluationTime + DustingTime + CleaningTime + ServisTime +
+#                     InventoryTime + GroupGoodsTime + (((InvoiceCnt * 2)*240)/3600) + OnlineOrdersTime + ActualTime +
+#                     ExchangeTime + ZReportTime as fact,
+#                     ReportCard as tabel,
+#                     fact / tabel as kpd
+#                     ,rank() over (order by kpd desc) as rating_kpd
+#             FROM
+#                     ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+#             WHERE
+#                     RDCName = (
+#                                 SELECT
+#                                 DISTINCT
+#                                         RDCName as rrs
+#                                 FROM
+#                                         ch_sv.KPD_Motivation_monitoring_of_staff_workload
+#                                 WHERE
+#                                         UserGuid = '{0}'
+#                                 )
+#             and
+#                     CurrentPeriod = toStartOfMonth(now())
+#         )
+#         SELECT
+#                 UserName, kpd, rating_kpd from kpd limit 5
+#         union all
+#         SELECT
+#                 UserName, kpd, rating_kpd from kpd where  UserGuid = '{0}'
+#         UNION all
+#         SELECT UserName, kpd, rating_kpd from kpd order by rating_kpd desc limit 5
+# """
+
+
+
+# 1. Вернет 3 значения: UserName, kpd, rating_kpd (kpd в текущем месяце).
+kpd_rating_user = """
+with kpd as 
+        (
+            SELECT 
+                    UserName,
+                    UserGuid,
+                    InvoiceTime + PaymentTime + ReturnTime + GivedTime + ArrivalTime + ShipmentTime + 
+                    BillingTime + WorkCarTime + ReevaluationTime + DustingTime + CleaningTime + ServisTime + 
+                    InventoryTime + GroupGoodsTime + (((InvoiceCnt * 2)*240)/3600) + OnlineOrdersTime + ActualTime +
+                    ExchangeTime + ZReportTime as fact,
+                    ReportCard as tabel,
+                    fact / tabel as kpd
+                    ,rank() over (order by kpd desc) as rating_kpd
+            FROM     
+                    ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+            WHERE
+                    RDCName = (
+                                SELECT 
+                                DISTINCT 
+                                        RDCName as rrs 
+                                FROM 
+                                        ch_sv.KPD_Motivation_monitoring_of_staff_workload
+                                WHERE
+                                        UserGuid = '{0}'
+                                )   
+            and 
+                    CurrentPeriod = toStartOfMonth(now())
+        )
+        SELECT 
+                UserName, kpd, rating_kpd 
+        FROM 
+                kpd 
+        WHERE
+                UserGuid = '{0}'
+"""
+
+# 2. Вернет массив: UserName, kpd, rating_kpd (Топ 5 пользователей по рейтингу в текущем месяце).
+top5_kpd_rating_users = """
+# with kpd as
+#         (
+#             SELECT
+#                     UserName,
+#                     UserGuid,
+#                     InvoiceTime + PaymentTime + ReturnTime + GivedTime + ArrivalTime + ShipmentTime +
+#                     BillingTime + WorkCarTime + ReevaluationTime + DustingTime + CleaningTime + ServisTime +
+#                     InventoryTime + GroupGoodsTime + (((InvoiceCnt * 2)*240)/3600) + OnlineOrdersTime + ActualTime +
+#                     ExchangeTime + ZReportTime as fact,
+#                     ReportCard as tabel,
+#                     fact / tabel as kpd
+#                     ,rank() over (order by kpd desc) as rating_kpd
+#             FROM
+#                     ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+#             WHERE
+#                     RDCName = (
+#                                 SELECT
+#                                 DISTINCT
+#                                         RDCName as rrs
+#                                 FROM
+#                                         ch_sv.KPD_Motivation_monitoring_of_staff_workload
+#                                 WHERE
+#                                         UserGuid = '{0}'
+#                                 )
+#             and
+#                     CurrentPeriod = toStartOfMonth(now())
+#         )
+#         SELECT
+#                 UserName, kpd, rating_kpd from kpd limit 5
+#         union all
+#         SELECT
+#                 UserName, kpd, rating_kpd from kpd where  UserGuid = '{0}'
+#         UNION all
+#         SELECT UserName, kpd, rating_kpd from kpd order by rating_kpd desc limit 5
+
+"""
+
+
+# 3. Вернет массив: UserName, kpd, rating_kpd (Топ 5 с конца пользователей по рейтингу в текущем месяце).
+top5end_kpd_rating_users = """
+with kpd as
+        (
+            SELECT
+                    UserName,
+                    UserGuid,
+                    InvoiceTime + PaymentTime + ReturnTime + GivedTime + ArrivalTime + ShipmentTime +
+                    BillingTime + WorkCarTime + ReevaluationTime + DustingTime + CleaningTime + ServisTime +
+                    InventoryTime + GroupGoodsTime + (((InvoiceCnt * 2)*240)/3600) + OnlineOrdersTime + ActualTime +
+                    ExchangeTime + ZReportTime as fact,
+                    ReportCard as tabel,
+                    fact / tabel as kpd
+                    ,rank() over (order by kpd desc) as rating_kpd
+            FROM
+                    ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+            WHERE
+                    RDCName = (
+                                SELECT
+                                DISTINCT
+                                        RDCName as rrs
+                                FROM
+                                        ch_sv.KPD_Motivation_monitoring_of_staff_workload
+                                WHERE
+                                        UserGuid = '{0}'
+                                )
+            and
+                    CurrentPeriod = toStartOfMonth(now())
+        )        
+        SELECT UserName, kpd, rating_kpd 
+        FROM
+            kpd 
+        order by 
+            rating_kpd desc limit 5
+"""
+
+
+
+
+
+# Вернет 2 значения
+# kpd_dinamic = """
+# select
+# 	kpd
+# ,
+# 	dt
+# from
+# 	(
+# 	select
+# 		CurrentPeriod as dt,
+# 		InvoiceTime + PaymentTime +
+# ReturnTime + GivedTime +
+# ArrivalTime + ShipmentTime +
+# BillingTime + WorkCarTime +
+# ReevaluationTime + DustingTime +
+# CleaningTime + ServisTime +
+# InventoryTime + GroupGoodsTime +
+# (((InvoiceCnt * 2)* 240)/ 3600)
+# + OnlineOrdersTime + ActualTime +
+# ExchangeTime + ZReportTime as fact,
+# 		ReportCard as tabel,
+# 		fact / tabel as kpd
+# 	from
+# 		ch_sv.KPD_Motivation_monitoring_of_staff_workload kpd
+# 	where
+# 		UserGuid = '{0}'
+# 		and CurrentPeriod >= date_add(month,
+# 		-6,
+# 		toStartOfMonth(now()))
+# 	) f
+# """
+
 
 
 # bot_table.tg in ({0})
